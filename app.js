df87f4b0ab2653b9dcac4616178899624121a4a6
Krystian Rosiński <krystian.rosinski@gmail.com>
2019-03-31 20:19:08 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,161 @@
+const Home = { template: `
+  <div>
+    <h1>Home</h1>
+    <p class="lead">Hi! My name is Krystian and this is my personal website.</p>
+    <p>I'm Civil Engineer from <a href="https://www.google.com/maps/search/?api=1&query=Toruń">Toruń</a>, Poland. I graduated University of Science and Technology in Bydgoszcz at Faculty of Civil and Environmental Engineering. After study I started work at general contractor company as cost estimator, and next – leader of cost estimation team and project engineer.</p>
+    <p>As a cost estimator, I made bill of quantities based on documentation, prepared calculations of the costs of labor, materials and equipments, sent questions to ordering parties and inquiries to subcontractors, as well as verified and selected offers.</p>
+    <p>As the leader of cost estimation team, I was responsible for preparing a complex bids for tenders for both – private investors and public sector. It included especially analysis of requirements, documentation, estimating a risks, and of course – leading a team, calculating, checking and optimizing costs, coordination between branches and completing  tender documents.</p>
+    <p>As a project engineer, I was responsible for keep within the timescale and budget of a project, and manage any delays or problem encountered on-site during a construction project. Among my main tasks were the preparation of production, negotiations, recommendations and construction contracts. Before site activity takes place I was responsible for managing communications between all parties involved in the on-site development of the project</p>
+    <p>I worked on very different projects – starting from warehouses, industry and commercial constructions, wind farms through public buildings and housing investments.</p>
+    <p>I also worked at the university as an assistant and conducted exercises in theoretical mechanics and computer-aided design.</p>
+  </div>
+  `
+}
+
+const Projects = { template: `
+  <div>
+    <h1>Projects</h1>
+    <p>Recent projects in which I was involved.</p>
+    <h2>Day <small class="text-muted">as a civil engineer</small></h2>
+    <h5>Lem-Bud</h5>
+    <ul>
+      <li>Awangarda housing estate in Toruń <span class="badge badge-info">2018-2019</span></li>
+      <li>Okólna 94 multi-family residential building in Toruń <span class="badge badge-info">2018-2019</span></li>
+      <li>Construction of kindergarten on the JAR estate in Toruń <span class="badge badge-info">2018</span> <span class="badge badge-secondary">PLN 9.9 mln</span></li>
+    </ul>
+    <h5>Alstal</h5>
+    <ul>
+      <li>Extension of the hospital building in Gniezno <span class="badge badge-info">2017-2018</span> <span class="badge badge-secondary">PLN 61.4 mln</span></li>
+      <li>Construction of a workshop building in the military complex in Jastrzębie <span class="badge badge-info">2016-2017</span> <span class="badge badge-secondary">PLN 15 mln</span></li>
+      <li>Construction of a fuel station with additional warehouses in the military air base in Łask <span class="badge badge-info">2015-2017</span> <span class="badge badge-secondary">PLN 13.9 mln</span></li>
+      <li>Renovation of a training building for the army in Wroclaw <span class="badge badge-info">2014-2016</span> <span class="badge badge-secondary">PLN 23.3 mln</span></li>
+      <li>Construction of a large-area high storage warehouse in the military complex in Kutno <span class="badge badge-info">2013-2015</span> <span class="badge badge-secondary">PLN 98.3 mln</span></li>
+      <li>Construction of four wind farms in the Błaszki commune <span class="badge badge-info">2011-2012</span> <span class="badge badge-secondary">PLN 58.2 mln</span></li>
+    </ul>
+    <h2>Night <small class="text-muted">as self-taught programmer</small></h2>
+    <ul>
+      <li><a href="https://github.com/krysros/marker">Marker</a> – App for <a href="https://en.wikipedia.org/wiki/General_contractor">GC</a> <span class="badge badge-info">2016-2019</span></li>
+    </ul>
+  </div>
+  `
+}
+
+const Publications = { template: `
+  <div>
+    <h1>Publications</h1>
+    <dl>
+    <dt>1</dt>
+    <dd>Rosiński K. and Delyavskyy M.
+    Rozwiązanie grubej płyty prostokątnej ortotropowej swobodnie podpartej na obwodzie.
+    <em>Mechanika Stosowana</em>, 2010.</dd>
+    <dt>2</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Modelowanie płyty grubej złożonej z trzech różnych warstw drewnianych.
+    In <em>Drewno i materiały drewnopochodne w konstrukcjach budowlanych</em>. Szczecin, 2011.
+    IX Konferencja Naukowa.</dd>
+    <dt>3</dt>
+    <dd>Н.В. Здолбіцька, М.В. Делявський, and К.&nbsp;Росінські.
+    Напружено-деформований стан товстої ортотропої прямокутної плити.
+    In <em>Наукові Нотатки</em>. Луцьк, 2011.</dd>
+    <dt>4</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Model matematyczny płyty ortotropowej jako części kratownicy mostowej.
+    In <em>III Międzynarodowa Konferencja Mostowa im. Rudolfa Modrzejewskiego</em>. Bydgoszcz, 2012.</dd>
+    <dt>5</dt>
+    <dd>Rosiński K. and Delyavskyy M.
+    Uwzględnienie ścinania i stanu tarczowego w modelu grubej płyty ortotropowej.
+    <em>Mechanika Stosowana</em>, 2012.</dd>
+    <dt>6</dt>
+    <dd>Delyavskyy M., Gołaś J., Olejniczak M., and Rosiński K.
+    Metoda rozwiązywania grubych płyt ortotropowych.
+    <em>Zagadnienia Mechaniki Stosowanej. Praca zbiorowa pod redakcją Jerzego Sawickiego</em>, 2013.</dd>
+    <dt>7</dt>
+    <dd>Podhorecki A., Delyavskyy M., Gołaś J., Buchaniec D., and Rosiński K.
+    Określenie przemieszczeń i sił wewnętrznych w elementach stalowej konstrukcji mostowej.
+    In <em>II Polsko-Ukraińska Międzynarodowa Konferencja, Aktualne Problemy Konstrukcji Metalowych</em>. Gdańsk, 2014.</dd>
+    <dt>8</dt>
+    <dd>Rosiński K., Delyavskyy M., and Sadivskyy V.
+    Analiza statyczna płyty izotropowej o grubości średniej.
+    <em>Mechanika Stosowana</em>, 2014.</dd>
+    <dt>9</dt>
+    <dd>Delyavskyy M., Lewandowski J., Buchaniec D., and Rosiński K.
+    Analiza statyczna cienkiej płyty anizotropowej.
+    <em>Zagadnienia Mechaniki Stosowanej. Praca zbiorowa pod redakcją Jerzego Sawickiego</em>, 2015.</dd>
+    <dt>10</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Rozwiązanie konstrukcji inżynierskich w ujęciu makroelementowym.
+    In <em>61. Konferencja Naukowa Komitetu Inżynierii Lądowej i Wodnej Polskiej Akademii Nauk oraz Komitetu Nauki Polskiego Związku Inżynierów i Techników Budownictwa</em>. Krynica, 2015.</dd>
+    <dt>11</dt>
+    <dd>Delyavskyy M., Niespodziana A., Olejniczak M., Grabowski A., and Rosiński K.
+    Analiza statyczna płyty izotropowej o średniej grubości.
+    <em>Wybrane zagadnienia konstrukcji i materiałów budowlanych oraz geotechniki. Wydawnictwa Uczelniane Uniwersytetu Technologiczno-Przyrodniczego w Bydgoszczy</em>, 2015.</dd>
+    <dt>12</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Rozwiązanie konstrukcji inżynierskich w ujęciu makroelementowym.
+    <em>Wybrane zagadnienia konstrukcji i materiałów budowlanych oraz geotechniki. Wydawnictwa Uczelniane Uniwersytetu Technologiczno-Przyrodniczego w Bydgoszczy</em>, 2015.</dd>
+    <dt>13</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Metoda rozwiązywania układów płytowo-kratowych.
+    <em>BRIDGES Tradition and Future. University Press University of Technology and Life Sciences in Bydgoszcz</em>, 2013.</dd>
+    <dt>14</dt>
+    <dd>Delyavskyy M., Niesopodziana A., Olejniczak M., Piotrowska E., and Rosiński K.
+    Określenie przemieszczeń i sił wewnętrznych w elementach stalowej konstrukcji płytowo-kratowej.
+    <em>MOSTY Tradycja i Nowoczesność. Wydawnictwa Uczelniane Uniwersytetu Technologiczno-Przyrodniczego w Bydgoszczy</em>, 2015.</dd>
+    <dt>15</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Analiza statyczna złożonych konstrukcji płytowych w ujęciu makroelementowym.
+    In <em>62. Konferencja Naukowa Komitetu Inżynierii Lądowej i Wodnej Polskiej Akademii Nauk oraz Komitetu Nauki Polskiego Związku Inżynierów i Techników Budownictwa</em>. Krynica, 2016.</dd>
+    <dt>16</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Analiza statyczno-wytrzymałościowa układu płytowo-kratowego w ujęciu makroelementowym.
+    In <em>V Międzynarodowa Konferencja Mostowa im. Rudolfa Modrzejewskiego</em>. Bydgoszcz, 2016.</dd>
+    <dt>17</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Analiza statyczna złożonych konstrukcji płytowych w ujęciu makroelementowym.
+    <em>Czasopismo Inżynierii Lądowej, Środowiska i Architektury</em>, 7 2016.
+    <a href="https://doi.org/10.7862/rb.2016.47">doi:10.7862/rb.2016.47</a>.</dd>
+    <dt>18</dt>
+    <dd>Rosiński K. and Delyavskyy M.
+    Nowa metoda rozwiązywania cienkich, nieprostokątnych, dowolnie obciążonych płyt.
+    <em>Mechanika Stosowana</em>, 2016.</dd>
+    <dt>19</dt>
+    <dd>М.&nbsp;Делявський and К.&nbsp;Росінські.
+    Метод розрахунку напружено-деформованого стану тонких криволінійних плит.
+    <em>Вісник Львівського національного аграрного університету. Архітектура і сільськогосподарське будівництво</em>, 17:176, 2016.</dd>
+    <dt>20</dt>
+    <dd>Delyavskyy M. and Rosiński K.
+    Solution of non-rectangular plates with macroelement method.
+    <em>AIP Conference Proceedings</em>, 2017.
+    <a href="https://doi.org/http://aip.scitation.org/doi/abs/10.1063/1.4977679">doi:http://aip.scitation.org/doi/abs/10.1063/1.4977679</a>.</dd>
+    <dt>21</dt>
+    <dd>Mykhaylo Delyavskyy, Krystian Rosiński, Nina Zdolbicka, and Oksana Bilash.
+    Macroelement analysis of thin orthotropic polygonal plate resting on the elastic winkler’s foundation.
+    <em>AIP Conference Proceedings</em>, 2077(1):020014, 2019.
+    URL: <a href="https://aip.scitation.org/doi/abs/10.1063/1.5091875">https://aip.scitation.org/doi/abs/10.1063/1.5091875</a>, <a href="https://arxiv.org/abs/https://aip.scitation.org/doi/pdf/10.1063/1.5091875">arXiv:https://aip.scitation.org/doi/pdf/10.1063/1.5091875</a>, <a href="https://doi.org/10.1063/1.5091875">doi:10.1063/1.5091875</a>.</dd>
+    </dl>
+  </div>
+  `
+}
+
+const Contact = { template: `
+  <div>
+    <h1>Email</h1>
+    <p>krystian dot rosinski at gmail dot com</p>
+  </div>
+  `
+}
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/projects', component: Projects },
+  { path: '/publications', component: Publications },
+  { path: '/contact', component: Contact },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
