194a85eeb1a12e6823238701612cace0f733bd85
Krystian Rosiński <krystian.rosinski@gmail.com>
2019-07-13 21:32:33 +0200

    Update home page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Home = { template: `
   <div class="jumbotron">
     <h1>Home</h1>
     <p class="lead">Hi! My name is Krystian and this is my personal website.</p>
-    <p>I'm Civil Engineer from <a href="https://www.google.com/maps/search/?api=1&query=Toruń">Toruń</a>, Poland. I graduated University of Science and Technology in Bydgoszcz at Faculty of Civil and Environmental Engineering. After study I started work at general contractor company as cost estimator, and next – leader of cost estimation team and project engineer. I participated in projects such as warehouses, industry and commercial constructions, wind farms, public buildings and housing investments.</p>
+    <p>I'm Civil Engineer from <a href="https://www.google.com/maps/search/?api=1&query=Toruń">Toruń</a>, Poland. I graduated University of Science and Technology in Bydgoszcz at Faculty of Civil and Environmental Engineering. After study I started work at general contractor company as cost estimator, and next – leader of cost estimation team and project engineer. I participated in projects such as warehouses, industry and commercial constructions, wind farms, public buildings and housing investments. The main scope of my work includes estimating, analyzing and optimizing costs and risks as well as contracting the execution of construction works. After hours I am interested in the theory of plates and modeling and solving engineering constructions.</p>
   </div>
   `
 }
